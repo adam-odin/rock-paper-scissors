c40821a934b5ea8ce17587276c5a2f025fb3834a
adam-odin <adam@edtec.io>
2024-08-29 23:49:10 +0100

    Create and test getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("hello world!");
+// console.log("testiiiing!");
+
+function getComputerChoice() {
+
+    const computerNumber = Math.floor( Math.random() * 3) + 1;
+    
+    switch (computerNumber) {
+        case 1: 
+            return "rock";
+            break; 
+        case 2: 
+            return "paper";
+            break;
+        case 3: 
+            return "scissors";
+            break;
+        default:
+            console.log("Sorry the random number generator (" + computerNumber + ") or the computer selection does not seem to be working");
+    }
+}
+
+computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+function getHumanChoice() {
+    prompt("say rock paper or scissors on 3: 1 2 3:")
+    
+}
