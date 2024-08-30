42635bbff70df0cdbeeb5c04886d9c56bf3564af
adam-odin <adam@edtec.io>
2024-08-30 01:00:23 +0100

    Test get humanChoice

--- script.js
+++ script.js
@@ -19,10 +19,42 @@ function getComputerChoice() {
     }
 }
 
-computerChoice = getComputerChoice();
-console.log(computerChoice);
+// let computerChoice = getComputerChoice();
+// console.log("Computer: "+computerChoice);
 
 function getHumanChoice() {
-    prompt("say rock paper or scissors on 3: 1 2 3:")
     
+    let humanChoice = prompt("do you choose rock paper or scissors?", "");
+    
+    let feedback = ( humanChoice.toLowerCase() === "rock" | humanChoice.toLowerCase() === "paper" | humanChoice.toLowerCase() === "scissors") ? "thanks you chose: "+humanChoice : "sorry that's not a valid weapon. please try again";
+    
+    alert(feedback);
+
+    return humanChoice;
 }
+
+// let humanChoice = getHumanChoice();
+// console.log("Human: "+humanChoice);
+
+const play = document.querySelector(".start");
+let counterComputer;
+let counterHuman; 
+let winner;
+
+function playGame() {
+    
+
+    const Computer = getComputerChoice();
+    const Human = getHumanChoice();
+    
+
+winner = counterHuman >= 5 ? "human" : ( counterComputer >= 5 ? "computer" : "");
+
+    if (counterComputer < 5 && counterHuman < 5) 
+        normal process
+
+
+    else alert("game over: "+if counterComputer=5, "computer"  
+}
+
+play.addEventListener("click",playGame);
